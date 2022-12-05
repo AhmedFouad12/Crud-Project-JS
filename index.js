c05624cf778ab6742734eb4a3cb3e2be4509d3a9
Ahmed Fouad <115884181+AhmedFouad12@users.noreply.github.com>
2022-12-05 04:27:34 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-var productNameInput = document.getElementById('productNameInput')
-var productPriceInput = document.getElementById('productPriceInput')
-var productCategoryInput = document.getElementById('productCategoryInput')
-var productDescInput = document.getElementById('productDescInput')
+let productNameInput = document.getElementById('productNameInput')
+let productPriceInput = document.getElementById('productPriceInput')
+let productCategoryInput = document.getElementById('productCategoryInput')
+let productDescInput = document.getElementById('productDescInput')
 
 productContainer = []
+
 if(localStorage.getItem('products') != null)
 {
     productContainer = JSON.parse(localStorage.getItem('products'));
@@ -14,7 +15,7 @@ function addProduct()
 {
     if(validProduct() == true)
     {
-    var product = 
+    let product = 
     {
         fullName:productNameInput.value,
         price:productPriceInput.value,
@@ -28,23 +29,25 @@ function addProduct()
     }
     else
     {
-        window.alert('Not Valid Input')
+        cartoona ='Not valid Product Information'
+        document.getElementById('x').innerHTML = cartoona
     }
+    
 }
 
 function displayProduct()
 {
-    var cartoona = ``;
-    for(var i = 0 ; i<productContainer.length ;i++)
+    let cartoona = ``;
+    for(let i = 0 ; i<productContainer.length ;i++)
     {
         cartoona+= `<tr>
-        <td>${[i]}</td>
+        <td>${[i+1]}</td>
         <td>${productContainer[i].fullName}</td>
         <td>${productContainer[i].price}</td>
         <td>${productContainer[i].category}</td>
         <td>${productContainer[i].desc}</td>
         <td><button class="btn btn-outline-danger" onclick="deleteProduct(${i})">Delete</button></td>
-        <td><button class="btn btn-outline-warning">Update</button></td>
+        <td><button class="btn btn-outline-warning" onclick="updateProduct(${i})">Update</button></td>
         </tr>`
     }
     document.getElementById('tableBody').innerHTML = cartoona
@@ -65,10 +68,25 @@ function deleteProduct(deletIndex)
     displayProduct()
 }
 
+function updateProduct(deletIndex)
+{
+    localStorage.setItem('products' , JSON.stringify(productContainer));
+    console.log(productContainer);
+    for(let i = 0 ; i<productContainer.length ;i++)
+    {
+        productNameInput.value = productContainer[i].fullName;
+        productPriceInput.value = productContainer[i].price;
+        productCategoryInput.value = productContainer[i].category;
+        productDescInput.value = productContainer[i].desc;
+    }
+    productContainer.splice(deletIndex,1);
+    displayProduct();
+}   
+
 function searchProduct(term)
 {
-    var cartoona = ``;
-    for(var i = 0 ; i<productContainer.length ;i++)
+    let cartoona = ``;
+    for(let i = 0 ; i<productContainer.length ;i++)
     {
         if(productContainer[i].fullName.toLowerCase().includes(term.toLowerCase()) == true)
         {
@@ -88,8 +106,11 @@ function searchProduct(term)
 
 function validProduct()
 {
-    var regex = /^[A-Z][a-z]{2,8}$/
-    if(regex.test(productNameInput.value) == true)
+    let regexName = /^([A-Z]|[a-z]){2,12}$/
+    let regexPrice = /^[0-9]{2,12}$/
+    let regexCat = /^([A-Z]|[a-z]){2,12}$/
+    let regexdes = /^([A-Z]|[a-z]){2,50}$/
+    if(regexName.test(productNameInput.value)&regexPrice.test(productPriceInput.value)&regexCat.test(productCategoryInput.value)&regexdes.test(productDescInput.value) == true)
     {
         return true
     }
